c133b1b67840b35273204712f6bf377e59ff6686
Animago6969 <emanuelly.brondani.lima@escola.pr.gov.br>
2024-06-27 23:56:50 +0000

    alterei index style

---
+++ script.js
@@ -0,0 +1,33 @@
+const caixaPrincipal     = documet.querySelector(".caixa-principal");   
+const caixaPerguntas     = documet.querySelector(".caixa-perguntas"); 
+const caixaAlternativas  = documet.querySelector(".caixa-alternativas");
+const caixaResultado     = documet.querySelector(".caixa-resultado");
+const texto              = documet.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "A disseminação de fake news pode ter graves consequências para a sociedade,  propagação de boatos, aumento da polarização política e o enfraquecimento da confiança nas instituições e na mídia. Você tem ciência dessas consequências ao compartilhar informações?",
+        alternativas [
+            "Não me preocupo com isso",
+            "Sim, com toda a certeza!"
+        ]
+    },
+    {
+        enunciado: "Como a desigualdade social afeta o acesso  a educação de qualidade?",
+        alternativas [
+            "Faz com que pessoas com maior poder aquisitivo tenha uma educação de maior qualidade.",
+            "Ela não afeta a educação,  pois todos têm acesso aos mesmos recursos."
+        ]
+    },
+
+        {
+            enunciado: "pergunta 3",
+            alternativas [
+                "alternativa1",
+                "alternativa2"
+            ]
+        }
+
+];
+
+
