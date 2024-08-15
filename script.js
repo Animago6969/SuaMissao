ec2970530f5778a9215fe90994412937ca10d034
Animago6969 <emanuelly.brondani.lima@escola.pr.gov.br>
2024-08-15 23:18:54 +0000

    aaa

--- script.js
+++ script.js
@@ -9,27 +9,55 @@ const perguntas = [
       
       enunciado: "A disseminação de fake news pode ter graves consequências para a sociedade,  propagação de boatos, aumento da polarização política e o enfraquecimento da confiança nas instituições e na mídia. Você tem ciência dessas consequências ao compartilhar informações?",
         alternativas: [
-            "Não me preocupo com isso",
-            "Sim, com toda a certeza!"
+        { texto: "Não me preocupo com isso",
+            
+            afirmacao: "Acredito na liberdade de expressão, todos têm o direito de expor ideias: sejam verdadeiras ou não."
+        },
+
+        {
+            texto: "Sim, com toda a certeza!",
+
+            afirmacao: "Além de toda as consequências que  a propagação de desinformação podem causar, há também a questão jurídica, pois dependendo da informação disseminada: pode caber processo."
+        }
         ]
     },
     {
+        
         enunciado: "Como a desigualdade social afeta o acesso  a educação de qualidade?",
         alternativas: [
-            "Faz com que pessoas com maior poder aquisitivo tenha uma educação de maior qualidade.",
-            "Ela não afeta a educação,  pois todos têm acesso aos mesmos recursos."
+        { texto: "Faz com que pessoas com maior poder aquisitivo tenha uma educação de maior qualidade.",
+            
+            afirmacao: "Pois, é um problema socioeconômico, no qual quem tem mais recursos tem acesso a tempo, cultura, e materiais de qualidade."
+        },
+
+        {
+            texto: "Ela não afeta a educação,  pois todos têm acesso aos mesmos recursos.",
+
+            afirmacao: "ndependente da condição social, com esforço (hardwork) todos podem alcançar seus objetivos."
+        }
         ]
     },
 
         {
-            enunciado: "Quais as causas das desigualdades sociais? A educação pode conscientizar sobre Fake News e Desigualdade Social?",
-            alternativas: [
-                "Distribuição de renda e baixa qualificação profissional. A educação pode ser a ferramenta para conscientizar a sociedade.",
-                "Falta de acessibilidade e utilização adequada da tecnologia para a qualificação profissional. A educação não interfere na conscientização da sociedade."
-            ]
+
+                enunciado: "Quais as causas das desigualdades sociais? A educação pode conscientizar sobre Fake News e Desigualdade Social?",
+                alternativas: [
+                { texto: "Distribuição de renda e baixa qualificação profissional. A educação pode ser a ferramenta para conscientizar a sociedade.",
+                    
+                    afirmacao: "O problema é  político, social e econômico com foco na  má distribuição de renda, consequentemente inoportunidades na  formação de qualificação, tanto profissional quanto pessoal. "
+                },
+        
+                {
+                    texto: "Falta de acessibilidade e utilização adequada da tecnologia para a qualificação profissional. A educação não interfere na conscientização da sociedade.",
+        
+                    afirmacao: "Dificuldade em conciliar trabalho com estudo. As barreiras que ainda existem em relação ao acesso a conexão e equipamentos que permitam essa tal formação."
+                }
+                ]
+            
+        
         }
 
-];
+    ];
 
 let perAtual = 0;
 let perguntaAtual;
@@ -46,7 +74,7 @@ mostra_pergunta();
  function mostraAlternativas(){
     for(const alternativas of perguntaAtual.alternativas){
         const BotaoAlternativas = document.createElement("button");
-    BotaoAlternativas.textContent = alternativas
+    BotaoAlternativas.textContent = alternativas.texto;
     caixaAlternativas.appendChild(BotaoAlternativas);
  }
  };
