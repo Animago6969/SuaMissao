5422f7f356d302a77ca697cb59a64d1974367a1c
Animago6969 <emanuelly.brondani.lima@escola.pr.gov.br>
2024-08-23 00:36:19 +0000

    correto!!!!!!!!!!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal     = document.querySelector(".caixa-principal");
 const caixaPerguntas     = document.querySelector(".caixa-perguntas"); 
 const caixaAlternativas  = document.querySelector(".caixa-alternativas");
 const caixaResultado     = document.querySelector(".caixa-resultado");
-const texto              = document.querySelector(".texto-resultado");
+const textoResultado     = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
@@ -10,54 +10,41 @@ const perguntas = [
       enunciado: "A disseminação de fake news pode ter graves consequências para a sociedade,  propagação de boatos, aumento da polarização política e o enfraquecimento da confiança nas instituições e na mídia. Você tem ciência dessas consequências ao compartilhar informações?",
         alternativas: [
         { texto: "Não me preocupo com isso",
-            
-            afirmacao: "Acredito na liberdade de expressão, todos têm o direito de expor ideias: sejam verdadeiras ou não."
+          afirmacao: "Acredito na liberdade de expressão, todos têm o direito de expor ideias: sejam verdadeiras ou não."
         },
-
         {
             texto: "Sim, com toda a certeza!",
-
             afirmacao: "Além de toda as consequências que  a propagação de desinformação podem causar, há também a questão jurídica, pois dependendo da informação disseminada: pode caber processo."
         }
         ]
     },
-    {
-        
+    {  
         enunciado: "Como a desigualdade social afeta o acesso  a educação de qualidade?",
         alternativas: [
         { texto: "Faz com que pessoas com maior poder aquisitivo tenha uma educação de maior qualidade.",
-            
-            afirmacao: "Pois, é um problema socioeconômico, no qual quem tem mais recursos tem acesso a tempo, cultura, e materiais de qualidade."
+          afirmacao: "Pois, é um problema socioeconômico, no qual quem tem mais recursos tem acesso a tempo, cultura, e materiais de qualidade."
         },
-
         {
             texto: "Ela não afeta a educação,  pois todos têm acesso aos mesmos recursos.",
-
             afirmacao: "ndependente da condição social, com esforço (hardwork) todos podem alcançar seus objetivos."
         }
         ]
     },
 
-        {
-
+    {
                 enunciado: "Quais as causas das desigualdades sociais? A educação pode conscientizar sobre Fake News e Desigualdade Social?",
                 alternativas: [
                 { texto: "Distribuição de renda e baixa qualificação profissional. A educação pode ser a ferramenta para conscientizar a sociedade.",
-                    
-                    afirmacao: "O problema é  político, social e econômico com foco na  má distribuição de renda, consequentemente inoportunidades na  formação de qualificação, tanto profissional quanto pessoal. "
+                   afirmacao: "O problema é  político, social e econômico com foco na  má distribuição de renda, consequentemente inoportunidades na  formação de qualificação, tanto profissional quanto pessoal. "
                 },
         
                 {
                     texto: "Falta de acessibilidade e utilização adequada da tecnologia para a qualificação profissional. A educação não interfere na conscientização da sociedade.",
-        
                     afirmacao: "Dificuldade em conciliar trabalho com estudo. As barreiras que ainda existem em relação ao acesso a conexão e equipamentos que permitam essa tal formação."
                 }
                 ]
-            
-        
-        }
-
-    ];
+    }
+];
 
 let perAtual = 0;
 let perguntaAtual;
@@ -71,7 +58,7 @@ function mostra_pergunta() {
     };
     
     perguntaAtual = perguntas[perAtual];
-    caixaPerguntas.textContent = perguntas[0].enunciado ;
+    caixaPerguntas.textContent = perguntaAtual.enunciado ;
     caixaAlternativas.textContent = "";
     textoResultado.textContent = "";
     mostraAlternativas ();  
@@ -79,10 +66,10 @@ function mostra_pergunta() {
 
 mostra_pergunta();
 
-function opcaoSelecionada (opcaoSelecionada){
+function resultadoSelecionado(opcaoSelecionada){
     const afirmacao = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacao + " ";
-    perguntaAtual ++;
+    perAtual++;
     mostra_pergunta();
 }
 
@@ -91,7 +78,7 @@ function opcaoSelecionada (opcaoSelecionada){
     for(const alternativas of perguntaAtual.alternativas){
         const BotaoAlternativas = document.createElement("button");
     BotaoAlternativas.textContent = alternativas.texto;
-    BotaoAlternativas.addEventListener("click",() => opcaoSelecionada(alternativas));
+    BotaoAlternativas.addEventListener("click",() => resultadoSelecionado(alternativas));
     caixaAlternativas.appendChild(BotaoAlternativas);
  }
  };
